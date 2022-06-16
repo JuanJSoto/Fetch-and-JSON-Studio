09d00d2024ef8c6016286326a647eccfbae73473
Juan J Soto <jj3185@hotmail.com>
2022-06-16 00:49:24 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// TODO: add code here
+// TODO: add code here 
+//for(astronaut of json)
+window.addEventListener("load", function() {
+    let json = [];
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            const container = document.getElementById("container");
+            // if($astronaut.active="true"){
+            //     astronaut.active.color = green;
+            // };            
+            for(astronaut of json){
+
+                container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src=${astronaut.picture} alt="astronauts"></img>
+                </div>
+                `; 
+            };
+        });
+    });
+});
